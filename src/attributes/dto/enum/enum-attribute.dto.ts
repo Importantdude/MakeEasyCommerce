fadddf0349e74752fb6345a121c46e1db50514e0
Daniel <dnaforsolutions@gmail.com>
2023-06-12 19:53:43 +0200

    fix eav enum file

--- src/attributes/dto/enum/enum-attribute.dto.ts
+++ src/attributes/dto/enum/enum-attribute.dto.ts
@@ -1,9 +1,1 @@
-export enum DefaultOrderProductAttributeDto {
-}
-
-export enum DefaultProductAttribtueNewsToDateCollectionDto {
-    attribute_code = 'news_to_date',
-    attribute_value = 'date to',
-    eav_collection_id = 5
-}
-
+export enum DefaultOrderProductAttributeDto {}
